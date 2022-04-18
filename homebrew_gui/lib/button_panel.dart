aa4a50304cfda8d8798db2e77d729a7344eb267b
braxtonhaynie <bhaynie@vols.utk.edu>
2022-04-18 18:54:33 -0400

    added install capability

--- homebrew_gui/lib/button_panel.dart
+++ homebrew_gui/lib/button_panel.dart
@@ -51,6 +51,25 @@ Future<List> _brewuninstall(String package) async {
   return [true, stdout];
 }
 
+Future<List> _brewinstall(String package) async {
+  var stdout = '';
+  var stderr = '';
+  var exitCode = -1;
+  await Process.run('brew', ['install', package]).then((ProcessResult pr) {
+    exitCode = pr.exitCode;
+    stdout = pr.stdout;
+    stderr = pr.stderr;
+    print(pr.exitCode);
+    print(pr.stdout);
+  });
+
+  if (exitCode == 1) {
+    print('Error: $stderr');
+    return [false, stderr];
+  }
+  return [true, stdout];
+}
+
 class ButtonPanel extends StatelessWidget {
   ButtonPanel({Key? key}) : super(key: key);
   // const ButtonPanel({Key? key}) : super(key: key);
@@ -66,7 +85,104 @@ class ButtonPanel extends StatelessWidget {
     return Column(
       children: <Widget>[
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () async {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        20.0,
+                      ),
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.only(
+                    top: 10.0,
+                  ),
+                  title: const Text(
+                    "Select what package you want to install",
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                  content: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "Select your package",
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: uninstallController,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter Package Name',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 60,
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              print('Selected package: ' +
+                                  uninstallController.text);
+                              await _brewinstall(uninstallController.text)
+                                  .then((List<dynamic> result) {
+                                if (result[0]) {
+                                  print('Install successful');
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      backgroundColor:
+                                          Color.fromARGB(254, 143, 162, 255),
+                                      content: Text('Install Successful'),
+                                      duration: Duration(milliseconds: 2500),
+                                      width: 500, // Width of the SnackBar.
+                                      behavior: SnackBarBehavior.floating,
+                                    ),
+                                  );
+                                } else {
+                                  print('Install failed');
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      backgroundColor: const Color.fromARGB(
+                                          254, 143, 162, 255),
+                                      content: Text(
+                                        'Install Failed: ' + result[1],
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      duration:
+                                          const Duration(milliseconds: 2500),
+                                      width: 500, // Width of the SnackBar.
+                                      behavior: SnackBarBehavior.floating,
+                                    ),
+                                  );
+                                  print(result[1]);
+                                }
+                              });
+                              clearText();
+                              Navigator.of(context).pop();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              // fixedSize: Size(250, 50),
+                            ),
+                            child: const Text(
+                              "Submit",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
           style: style1,
           child: const Text('Download Packages'),
         ),
